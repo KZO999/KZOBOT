e3593aab056c3bec8e10376491ebcd953e9dd577
Glitch (vaulted-fir-professor) <none>
2021-06-07 12:33:51 +0000

    ❄️🍇 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.Prefix = `KZ|`;//your prefix for bot
+exports.Token = `ODE5MDMxNTI1MjQ4MzM1ODcy.YEgsjA.pw3ya2qH0JnrDE1mDIiJtyE8x-s`;//your DISCORD TOKEN
+exports.Color = `BLUE`;//color   of embed
